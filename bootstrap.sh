efbf57e080c213d1d4861a9efdcb343cee32ea43
Lim Chun Leng <limchunleng@gmail.com>
2022-10-24 09:14:22 +0900

    Fix bootstrap bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -eu
 
 APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd  )"
 
+mkdir -p ${HOME}/.config/lf
 ln -sfn ${APP_PATH}/lfrc ${HOME}/.config/lf
 ln -sfn ${APP_PATH}/icons ${HOME}/.config/lf
 ln -sfn ${APP_PATH}/previewer.sh ${HOME}/.config/lf
